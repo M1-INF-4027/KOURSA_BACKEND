fc52818514574bd0a056f89cf50c238edaaea7de
ytsemegne <ytsemegne@irex.aretex.ca>
2026-01-02 20:35:40 +0100

    Correction du fichier de migrations(super user).

--- koursa/users/migrations/0002_create_superuser.py
+++ koursa/users/migrations/0002_create_superuser.py
@@ -1,22 +1,26 @@
 from django.db import migrations
-import os 
+import os
 
 def create_superuser(apps, schema_editor):
     Utilisateur = apps.get_model('users', 'Utilisateur')
 
-    
     SUPERUSER_EMAIL = os.environ.get('SUPERUSER_EMAIL')
     SUPERUSER_PASSWORD = os.environ.get('SUPERUSER_PASSWORD')
 
     if SUPERUSER_EMAIL and SUPERUSER_PASSWORD:
         if not Utilisateur.objects.filter(email=SUPERUSER_EMAIL).exists():
             print(f"Création du super-utilisateur avec l'email : {SUPERUSER_EMAIL}")
-            Utilisateur.objects.create_superuser(
+            user = Utilisateur.objects.create(
                 email=SUPERUSER_EMAIL,
-                password=SUPERUSER_PASSWORD,
-                first_name='Admin', 
-                last_name='Koursa'  
+                first_name='Admin',
+                last_name='Koursa',
+                is_staff=True,       
+                is_superuser=True,  
+                statut='ACTIF'       
             )
+            user.set_password(SUPERUSER_PASSWORD)
+            user.save()
+            
         else:
             print(f"Le super-utilisateur avec l'email {SUPERUSER_EMAIL} existe déjà.")
     else:
